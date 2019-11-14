8767b2df03bbc6b25f8425cf70936da34074e51a
pavel <pavelsvih@gmail.com>
2019-11-14 12:52:57 +0100

    Přidána vertikální projekce histogramu

--- src/opencvtest/CalcColumnHist.java
+++ src/opencvtest/CalcColumnHist.java
@@ -1,14 +1,9 @@
 package opencvtest;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.HighGui;
@@ -17,8 +12,6 @@ import org.opencv.imgproc.Imgproc;
 
 class CalcCHist {
     public void run(String[] args) {
-        int sirka = 240;
-        int vyska = 320;
 
         String filename = args.length > 0 ? args[0] : ("disp3.jpg");
         Mat src = Imgcodecs.imread(filename);
@@ -30,7 +23,7 @@ class CalcCHist {
 
     Mat horizontal = new Mat(src.rows()+1, src.cols(), CvType.CV_8UC1);//horizontal histogram
 	horizontal.setTo(Scalar.all(0));
-    Mat vertical = new Mat(1, src.rows()+1,CvType.CV_8UC1);//horizontal histogram
+    Mat vertical = new Mat(src.rows()+1, src.cols(),CvType.CV_8UC1);//vertikalni histogram
 	vertical.setTo(Scalar.all(0));
     Mat converted = new Mat(src.width(), src.height(), CvType.CV_8UC1);
      Imgproc.cvtColor(src, converted, Imgproc.COLOR_RGB2GRAY);
@@ -48,11 +41,21 @@ class CalcCHist {
 //        System.out.println("hodnota "+i+" : " +sum);
 	}
 
+	for(int i=0;i<(converted.rows());i++)
+	{
+        sum = Core.countNonZero(converted.submat(new Rect(0, i, converted.cols(), 1)));
+        
+        for(int j=0;j<sum;j++) {
+            vertical.put(i, j, 255);
+        }
+//        System.out.println("hodnota "+i+" : " +sum);
+	}
+
     //! [Display]
         HighGui.imshow( "Originál obrázku", src );
-        HighGui.imshow( "konvertovnany obrázku", converted );
+        HighGui.imshow( "konvertovnany obrázek", converted );
         HighGui.imshow( "Horizontalni histogram", horizontal);
-//        HighGui.imshow( "Vertikalni histogram", vertical );
+        HighGui.imshow( "Vertikalni histogram", vertical );
         HighGui.waitKey(0);
         //! [Display]
 
